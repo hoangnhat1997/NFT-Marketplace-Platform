432d4d66bfbe33cb210de1bfed70df0d07e17ade
NhatPham <phamvuonghoangnhat@gmail.com>
2024-05-11 07:14:00 +0700

    search input in flatform page

--- client/src/app/platform/page.tsx
+++ client/src/app/platform/page.tsx
@@ -3,20 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Flatform = () => {
   return (
-    <div className="h-max w-screen flex flex-col justify-center items-center bg-gray-100">
+    <div className="h-max w-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="flex justify-center items-center">
         <p className="text-3xl font-bold">
           <span className="text-black">PICK YOUR </span>
           <span className="text-rose-400">PLATFORM</span>
         </p>
       </div>
-      <div className="my-4 bg-gray-200 rounded-xl">
-        <p className="p-2 text-black font-weight-700 ">17 NFT Platforms</p>
+      <div className="my-4 flex bg-gray-200 rounded-2xl justify-center items-center">
+        <p className="p-2 text-black font-weight-700">17 NFT Platforms</p>
       </div>
-
-      <div className="mx-40 w-full flex items-center justify-center bg-gray-200 rounded-full text-gray-600 focus-within:text-gray-400">
+      <div className="w-[800px] flex flex-row items-center justify-center bg-gray-200 rounded-full text-gray-600 focus-within:text-gray-400">
         <FontAwesomeIcon
-          className="mx-4"
+          className="ml-20"
           color="black"
           icon={faMagnifyingGlass}
         />
