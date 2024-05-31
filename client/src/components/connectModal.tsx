a01e0a312ed7ef88929b1024fefffedabf9679c2
NhatPham <phamvuonghoangnhat@gmail.com>
2024-05-31 15:23:20 +0700

    UI modal connect hover

--- client/src/components/connectModal.tsx
+++ client/src/components/connectModal.tsx
@@ -1,12 +1,37 @@
 "use client";
 
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+
 type ConnectModalProps = {
   closeModal: () => void;
 };
 
 const ConnectModal = (props: ConnectModalProps) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        props.closeModal();
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [props, props.closeModal]);
+
   return (
-    <div className="absolute top-24 right-[160px] flex flex-col items-center justify-center z-30 bg-white p-6 rounded-xl w-80">
+    <div
+      ref={modalRef}
+      className="absolute top-24 right-[160px] flex flex-col items-center justify-center z-30 bg-white p-6 rounded-xl w-80"
+    >
       <p className="mb-4 text-xl text-black font-medium">Select a wallet</p>
       <p className="text-xs mb-4 text-center">
         <span className="text-gray-500">
@@ -16,39 +41,56 @@ const ConnectModal = (props: ConnectModalProps) => {
         <span className="text-gray-500"> and our </span>
         <span className="text-black"> Privacy Policy </span>
       </p>
-      <button
-        onClick={() => alert("Connecting to Metamask...")}
-        className="mb-2 px-4 py-2 text-white bg-blue-500 rounded w-full"
-      >
-        <div>
-          <p> MetaMask</p>
+      <div className="mb-2 px-4 py-2 text-white bg-blue-500 rounded w-full bg-white">
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-row items-center">
+            <Image
+              src={"/images/metamask.png"}
+              width={40}
+              height={40}
+              alt="Picture"
+            />
+            <p className="ml-2 text-black"> MetaMask</p>
+          </div>
+          <button onClick={() => alert("Connecting to Metamask...")}>
+            <p className="text-blue-500">connect</p>
+          </button>
         </div>
-      </button>
+      </div>
 
-      <button
-        onClick={() => alert("Connecting to Coinbase...")}
-        className="mb-2 px-4 py-2 text-white bg-blue-500 rounded w-full"
-      >
-        <div>
-          <p>Coinbase</p>
+      <div className="mb-2 px-4 py-2 text-white bg-blue-500 rounded w-full bg-white">
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-row items-center">
+            <Image
+              src={"/images/coinbase.png"}
+              width={40}
+              height={40}
+              alt="Picture"
+            />
+            <p className="ml-2 text-black">Coinbase Wallet</p>
+          </div>
+          <button onClick={() => alert("Connecting to Coinbase...")}>
+            <p className="text-blue-500">connect</p>
+          </button>
         </div>
-      </button>
+      </div>
 
-      <button
-        onClick={() => alert("Connecting to other wallet...")}
-        className="mb-2 px-4 py-2 text-white bg-blue-500 rounded w-full"
-      >
-        <div>
-          <p>Connect Wallet</p>
+      <div className="mb-2 px-4 py-2 text-white bg-blue-500 rounded w-full bg-white">
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-row items-center">
+            <Image
+              src={"/images/connectWallet.png"}
+              width={40}
+              height={40}
+              alt="Picture"
+            />
+            <p className="ml-2 text-black">WalletConnect</p>
+          </div>
+          <button onClick={() => alert("Connecting to other wallet...")}>
+            <p className="text-blue-500">connect</p>
+          </button>
         </div>
-      </button>
-
-      <button
-        onClick={props.closeModal}
-        className="mt-4 px-4 py-2 text-grey-500 border border-grey-500 rounded w-full text-black"
-      >
-        Close
-      </button>
+      </div>
     </div>
   );
 };
