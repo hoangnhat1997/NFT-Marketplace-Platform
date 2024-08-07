424015f119634bec4581bd88a85b682548f8ee7e
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2024-08-07 23:28:22 +0700

    change version of soldility into token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,7 +2,7 @@
 
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
 
 // This is the main building block for smart contracts.
