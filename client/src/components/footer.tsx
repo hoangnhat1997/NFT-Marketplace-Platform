2728b78dc92f3b63c8c3f302bfa8a26a5e58dde6
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2024-11-26 23:57:59 +0700

    responsive mobile footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -14,43 +14,47 @@ const Footer = () => {
   const router = useRouter();
 
   return (
-    <div className="px-40 py-8 bg-gray-100 flex flex-row justify-between items-center py-6">
-      <div className="flex flex-row">
-        <p className="font-bold text-black">Lets find what you need</p>
-        <div className=" w-1 mx-4 h-6 bg-gray-200 rounded-xl"></div>
-        <p className="text-blue-400">How it works?</p>
+    <div className="px-6 sm:px-10 md:px-20 lg:px-40 py-8 bg-gray-100 flex flex-col md:flex-row justify-between items-center">
+      <div className="flex flex-col sm:flex-row items-center mb-4 md:mb-0">
+        <p className="font-bold text-black text-center sm:text-left">
+          Lets find what you need
+        </p>
+        <div className="w-1 h-6 bg-gray-200 rounded-xl mx-0 sm:mx-4 my-2 sm:my-0"></div>
+        <p className="text-blue-400 text-center sm:text-left">How it works?</p>
       </div>
+
       <div className="flex flex-row">
         <p
-          className="ml-8 font-bold hover:text-sky-500 cursor-pointer"
+          className="ml-0 sm:ml-8 font-bold hover:text-sky-500 cursor-pointer text-center sm:text-left"
           onClick={() => router.push("categories")}
         >
           <span className="text-rose-400">01</span>
           <span className="ml-2 text-black">Category</span>
         </p>
-        <div className="w-1 mx-4 h-6 bg-gray-200 rounded-xl"></div>
+        <div className="w-1 h-6 bg-gray-200 rounded-xl mx-4"></div>
         <p
-          className="font-bold hover:text-sky-500 cursor-pointer"
+          className="font-bold hover:text-sky-500 cursor-pointer text-center sm:text-left"
           onClick={() => router.push("platform")}
         >
           <span className="text-rose-400">02</span>
           <span className="ml-2 text-black">Platforms</span>
         </p>
-        <div className="w-1 mx-4 h-6 bg-gray-200 rounded-xl"></div>
-        <p className="font-bold hover:text-sky-500 cursor-pointer">
+        <div className="w-1 h-6 bg-gray-200 rounded-xl mx-4"></div>
+        <p className="font-bold hover:text-sky-500 cursor-pointer text-center sm:text-left">
           <span className="text-rose-400">03</span>
           <span className="ml-2 text-black">Browse</span>
         </p>
       </div>
-      <div className="flex flex-row">
+
+      <div className="flex flex-row items-center justify-center">
         <div className="hover:bg-gray-400 bg-gray-200 rounded-3xl py-2 px-6 flex flex-row justify-center items-center cursor-pointer">
           <FontAwesomeIcon fontSize={10} color="black" icon={faAngleLeft} />
-          <p className="ml-2 text-black font-medium ">Prev Step</p>
+          <p className="ml-2 text-black font-medium">Prev Step</p>
         </div>
-        <div className="hover:bg-gray-400 ml-8 bg-gray-200 rounded-3xl py-2 px-6 flex flex-row justify-center items-center cursor-pointer">
+        <div className="hover:bg-gray-400 bg-gray-200 rounded-3xl py-2 px-6 flex flex-row justify-center items-center cursor-pointer sm:mt-0 sm:ml-8">
           <p className="text-black font-medium">Next Step</p>
           <FontAwesomeIcon
-            className="ml-2"
+            // className="ml-2"
             fontSize={10}
             color="black"
             icon={faAngleRight}
