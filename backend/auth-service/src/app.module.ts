d2b66fa3ff13747ebd74d01cf48f61d6be1d00af
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2024-12-01 21:58:25 +0700

    add backend auth service

---
+++ backend/auth-service/src/app.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import config from './config';
+
+@Module({
+  imports: [
+    AuthModule,
+    ClientsModule.register([
+      {
+        name: config().services.transaction.name,
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: config().services.auth.clientId,
+            brokers: [config().broker],
+          },
+          consumer: {
+            groupId: config().services.transaction.groupId,
+          },
+        },
+      },
+      {
+        name: config().services.transaction.name,
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: config().services.auth.clientId,
+            brokers: [config().broker],
+          },
+          consumer: {
+            groupId: config().services.image.groupId,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/auth-service/src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka,
+  ) {}
+
+  async registerUser(userData: any) {
+    // Save user in DB
+    const user = await this.prisma.user.create({ data: userData });
+
+    // Emit user created event
+    this.kafkaClient.emit('user.created', { id: user.id, email: user.email });
+    return user;
+  }
+}
