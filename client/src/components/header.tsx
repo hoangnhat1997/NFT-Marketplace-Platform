72626ca82cebaa640ba3c92ef35585abc0d80ddc
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2024-12-16 23:26:09 +0700

    update

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -45,62 +45,64 @@ const Header = () => {
     );
   }
   return (
-    <div className="py-8 bg-gray-100 flex justify-center md:justify-between items-center px-40 mx-auto w-auto max-w-screen-2xl">
-      <div className="hidden md:flex flex-row justify-between items-center text-black">
-        <div className="flex flex-row justify-center items-center">
-          <FontAwesomeIcon color="black" icon={faHouse} />
-          <p className="mr-0 ml-4">Meta Market</p>
-        </div>
-        <div className=" w-1 mx-4 h-6 bg-gray-200 rounded-xl"></div>
-        <p
-          className="mx-4 hover:text-sky-500 cursor-pointer"
-          onClick={() => router.push("/explore")}
-        >
-          Explore
-        </p>
-        <p
-          className="mx-4 hover:text-sky-500 cursor-pointer"
-          onClick={() => router.push("discover")}
-        >
-          Discover
-        </p>
-        <p
-          className="mx-4 hover:text-sky-500 cursor-pointer"
-          onClick={() => router.push("categories")}
-        >
-          Categories
-        </p>
-        <div className="w-1 mx-4 h-6 bg-gray-200 rounded-xl"></div>
-        <FontAwesomeIcon
-          className="mx-4"
-          color="black"
-          icon={faMagnifyingGlass}
-        />
-      </div>
-      <div className="flex flex-row justify-center items-center">
-        <div className="flex flex-row bg-gray-200 hover:bg-blue-400 hover:text-blue-900  justify-center items-center px-2 rounded-2xl mr-4">
-          <FontAwesomeIcon color="black" icon={faWallet} />
-          <button
-            onClick={() => {}}
-            className="py-2 px-4 transition duration-200 ease-in-out transform hover:scale text-black font-weight-700"
-          >
-            Wallet
-          </button>
-        </div>
-        <div className=" w-1 h-6 bg-gray-200 rounded-xl"></div>
-        <div className="flex flex-row justify-center items-center px-2">
-          <div className="h-12 w-12  flex justify-center items-center p-2">
-            <FontAwesomeIcon color="black" icon={faUserTie} />
+    <div className="w-screen bg-gray-100">
+      <div className="max-w-screen-2xl py-8 bg-white lg:px-40 flex justify-center md:justify-between items-center mx-auto w-auto">
+        <div className="hidden md:flex flex-row justify-between items-center text-black">
+          <div className="flex flex-row justify-center items-center">
+            <FontAwesomeIcon color="black" icon={faHouse} />
+            <p className="mr-0 ml-4">Meta Market</p>
           </div>
-          <p className="ml-2 text-black font-weight-700">Alexander</p>
+          <div className=" w-1 mx-4 h-6 bg-gray-200 rounded-xl"></div>
+          <p
+            className="mx-4 hover:text-sky-500 cursor-pointer"
+            onClick={() => router.push("/explore")}
+          >
+            Explore
+          </p>
+          <p
+            className="mx-4 hover:text-sky-500 cursor-pointer"
+            onClick={() => router.push("discover")}
+          >
+            Discover
+          </p>
+          <p
+            className="mx-4 hover:text-sky-500 cursor-pointer"
+            onClick={() => router.push("categories")}
+          >
+            Categories
+          </p>
+          <div className="w-1 mx-4 h-6 bg-gray-200 rounded-xl"></div>
+          <FontAwesomeIcon
+            className="mx-4"
+            color="black"
+            icon={faMagnifyingGlass}
+          />
         </div>
-        <button
-          className="ml-4 h-14 md:h-10 px-4 bg-blue-600 hover:bg-blue-400 rounded-3xl flex justify-center items-center"
-          onClick={openModal}
-        >
-          Connect Wallet
-        </button>
-        {isOpen && <ConnectModal closeModal={closeModal} />}
+        <div className="flex flex-row justify-center items-center">
+          <div className="flex flex-row bg-gray-200 hover:bg-blue-400 hover:text-blue-900  justify-center items-center px-2 rounded-2xl mr-4">
+            <FontAwesomeIcon color="black" icon={faWallet} />
+            <button
+              onClick={() => {}}
+              className="py-2 px-4 transition duration-200 ease-in-out transform hover:scale text-black font-weight-700"
+            >
+              Wallet
+            </button>
+          </div>
+          <div className=" w-1 h-6 bg-gray-200 rounded-xl"></div>
+          <div className="flex flex-row justify-center items-center px-2">
+            <div className="h-12 w-12  flex justify-center items-center p-2">
+              <FontAwesomeIcon color="black" icon={faUserTie} />
+            </div>
+            <p className="ml-2 text-black font-weight-700">Alexander</p>
+          </div>
+          <button
+            className="ml-4 h-14 md:h-10 px-4 bg-blue-600 hover:bg-blue-400 rounded-3xl flex justify-center items-center"
+            onClick={openModal}
+          >
+            Connect Wallet
+          </button>
+          {isOpen && <ConnectModal closeModal={closeModal} />}
+        </div>
       </div>
     </div>
   );
