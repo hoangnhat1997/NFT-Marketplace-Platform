a9f82c216bc8dc686b5c87d92fbd2afdadc227d1
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2025-01-11 22:57:10 +0700

    create avatar circle

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 
 const Profile = () => {
   return (
-    <div className="w-screen bg-gray-100">
-      <div className="relative h-auto w-screen bg-gray-200 flex justify-center items-center">
+    <div className="w-screen h-screen bg-gray-100">
+      <div className="relative  bg-gray-200 flex justify-center items-center">
         <Image
           src={
             "https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2.png"
           }
           width={2000}
-          height={1000}
+          height={500}
           alt="cover"
         />
-        <div className="absolute top-[200px] left-[20px] w-full flex">
+        <div className="absolute top-[250px] left-[20px] w-full flex">
           <div className="w-1/3 flex flex-col justify-center items-center">
-            <div className="w-32 h-32 rounded-full bg-gray-200 flex justify-center items-center">
+            <div className="w-32 h-32 rounded-full bg-gray-200 flex justify-center items-center overflow-hidden">
               <Image
                 src={
                   "https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2.png"
@@ -23,7 +23,7 @@ const Profile = () => {
                 width={200}
                 height={200}
                 alt="profile"
-                style={{ objectFit: "contain" }}
+                style={{ objectFit: "cover" }}
               />
             </div>
             <p className="font-bold text-2xl mt-4">Alexander</p>
