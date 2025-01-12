90fa327ad67ab908ba5568b58c1b3747dc521c40
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2025-01-12 11:07:53 +0700

    update avatar profile

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -3,34 +3,32 @@ import Image from "next/image";
 
 const Profile = () => {
   return (
-    <div className="w-screen h-screen bg-gray-100">
-      <div className="relative  bg-gray-200 flex justify-center items-center">
+    <div className="w-screen h-screen relative bg-gray-100">
+      <div className="absolute top-0 w-full h-2/5 bg-gray-200 flex justify-center items-center">
         <Image
-          src={
-            "https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2.png"
-          }
-          width={2000}
-          height={500}
+          src="https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2.png"
+          layout="fill" // This ensures the image fills the container
+          objectFit="cover" // Makes sure the image covers the container without stretching
           alt="cover"
         />
-        <div className="absolute top-[250px] left-[20px] w-full flex">
-          <div className="w-1/3 flex flex-col justify-center items-center">
-            <div className="w-32 h-32 rounded-full bg-gray-200 flex justify-center items-center overflow-hidden">
-              <Image
-                src={
-                  "https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2.png"
-                }
-                width={200}
-                height={200}
-                alt="profile"
-                style={{ objectFit: "cover" }}
-              />
-            </div>
-            <p className="font-bold text-2xl mt-4">Alexander</p>
-            <p className="text-gray-400">Software Engineer</p>
-            <p className="text-gray-400">New York, NY</p>
-            <p className="text-gray-400">Joined in 2020</p>
+      </div>
+      <div className="absolute top-[250px] left-[10px] w-full flex">
+        <div className="w-1/3 flex flex-col justify-center items-center">
+          <div className="w-32 h-32 rounded-full bg-gray-200 flex justify-center items-center overflow-hidden">
+            <Image
+              src={
+                "https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2.png"
+              }
+              width={200}
+              height={200}
+              alt="profile"
+              style={{ objectFit: "cover" }}
+            />
           </div>
+          <p className="font-bold text-2xl mt-4 text-black">Alexander</p>
+          <p className="text-gray-700">Software Engineer</p>
+          <p className="text-gray-700">New York, NY</p>
+          <p className="text-gray-700">Joined in 2027</p>
         </div>
       </div>
     </div>
