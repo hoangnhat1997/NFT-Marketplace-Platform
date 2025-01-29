35c538056486fe1540b3610e0fc37f8eaad507e1
hoangnhat1997 <phamvuonghoangnhat@gmail.com>
2025-01-29 10:18:44 +0700

    update setup service

--- backend/transaction-service/src/kafka/kafka.module.ts
+++ backend/transaction-service/src/kafka/kafka.module.ts
@@ -1,24 +1,7 @@
 import { Module } from '@nestjs/common';
 import { KafkaService } from './kafka.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'KAFKA_SERVICE',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ['kafka:9092'],
-          },
-          consumer: {
-            groupId: 'transaction-service-consumer', // Unique group id for the service
-          },
-        },
-      },
-    ]),
-  ],
   providers: [KafkaService],
   exports: [KafkaService],
 })
